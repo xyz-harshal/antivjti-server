26f8e6bc4338cb025c64f7895f84752269fb17b3
harshal-ishere <harshal011kamble@gmail.com>
2024-01-06 02:45:53 +0530

    did some authentication things

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,44 +1,57 @@
-import userModel from "../models/userSchema.js";
+import userModel from "../models/userSchema.js"
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken";
+const saltRounds=10
+
+let createToken=(_id)=>{
+  return jwt.sign({_id},process.env.SECRET)
+}
+
 
 export async function login(req, res) {
   let { email, password } = req.body;
   try {
-    let data = await userModel.findOne({ email: email });
+    let data = await userModel.findOne({ email: email })
     if (!data) {
-      res.json({ status: "username not found" });
+      res.json("NOemail")
     }
     if (data) {
-      if (data.password == password) {
-        res.json({ status: "logged in" });
+      let pass=bcrypt.compareSync(password,data.password)
+      if (pass) {
+        res.json("loggedIN")
       }
       else {
-        res.json({ status: "wrong password" })
+        res.json("NOpassword");
       }
     }
   }
   catch (e) {
-    console.log(e.message);
+    console.log(e.message)
     res.json({ status: e.message })
   }
 }
 
 export async function register(req, res) {
-  let { email, password } = req.body;
+  let { email, password } = req.body
   try {
-    let data = await userModel.findOne({ email:email });
+    let data = await userModel.findOne({ email:email })
     if (data) {
-      res.json({ status: "account already exit" })
+      // throw Error("Username already in use")
+      res.json({status:"already in use"})
     }
     if (!data) {
+      //hashing and storing in DB
+      const hash=bcrypt.hashSync(password,saltRounds);
       let user = new userModel({
         email:email,
-        password:password
+        password:hash
       })
       await user.save();
-      res.json({status:"registered nicely"})
+      let token=createToken(user._id)
+      res.status(200).json({email,token});
     }
   }
   catch (e) {
-    res.status(500).json({ message: e.message });
+    res.status(500).json({ message: e.message })
   }
 }
