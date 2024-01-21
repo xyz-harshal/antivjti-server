cea7fdba4ed448107c0bf60db5f6a0b4da782c24
harshal-ishere <harshal011kamble@gmail.com>
2024-01-21 14:57:39 +0530

    refactoring is done fine as hell

--- controllers/voteController.js
+++ controllers/voteController.js
@@ -6,9 +6,11 @@ import replyModel from "../models/replySchema.js";
 export async function upVote(req, res) {
   const uidresponse = req.body.userID;
   const postID = req.body.postID;
+  let isReply=req.body.isReply
   let userID = jwt.verify(uidresponse, process.env.SECRET)._id;
   try {
-    var post = await postModel.findOne({ _id: postID });
+    if(isReply==true) var post = await replyModel.findOne({ _id: postID });
+    if(isReply==false) var post = await postModel.findOne({ _id: postID });
     var user = await userModel.findOne({ _id: userID });
   } catch (e) {
     console.log(e.message);
@@ -46,9 +48,11 @@ export async function upVote(req, res) {
 export async function downVote(req, res) {
   const uidresponse = req.body.userID;
   const postID = req.body.postID;
+  let isReply=req.body.isReply
   let userID = jwt.verify(uidresponse, process.env.SECRET)._id;
   try {
-    var post = await postModel.findOne({ _id: postID });
+    if(isReply==true) var post = await replyModel.findOne({ _id: postID });
+    if(isReply==false) var post = await postModel.findOne({ _id: postID });
     var user = await userModel.findOne({ _id: userID });
   } catch (e) {
     console.log(e.message);
@@ -83,37 +87,16 @@ export async function downVote(req, res) {
   }
 }
 
-// export async function Check(req, res){
-//   const uidresponse = req.body.userID;
-//   const postID = req.body.postID;
-//   let userID = jwt.verify(uidresponse, process.env.SECRET)._id;
-//   try {
-//     var post = await postModel.findOne({ _id: postID });
-//     var user = await userModel.findOne({ _id: userID });
-//   } catch (e) {
-//     console.log(e.message);
-//     await res.status(500).json("error detected");
-//   }
-//   if (post.downvoteIds.includes(user._id)){
-//     res.json({value:-1})
-//   }
-//   else if (post.upvoteIds.includes(user._id)){
-//     res.json({value: 1})
-//   }
-//   else{
-//     res.json({value: 0})
-//   }
-// }
 export async function Check(req, res) {
   const uidresponse = req.body.userID;
   const postID = req.body.postID;
+  let isReply=req.body.isReply
   let userID;
   try {
     userID = jwt.verify(uidresponse, process.env.SECRET)._id;
-    console.log(postID)
-    var post = await postModel.findOne({ _id: postID });
+    if(isReply==true) var post = await replyModel.findOne({ _id: postID });
+    if(isReply==false) var post = await postModel.findOne({ _id: postID });
     var user = await userModel.findOne({ _id: userID });
-
     if (!post || !user) {
       return res.json({ error: "Post or user not found" });
     }
@@ -128,108 +111,5 @@ export async function Check(req, res) {
     res.json({ value: 1 });
   } else {
     res.json({ value: 0 });
-  }
-}
-
-
-export async function upVoteReply1(req, res) {
-  const uidresponse = req.body.userID;
-  const postID = req.body.postID;
-  let userID = jwt.verify(uidresponse, process.env.SECRET)._id;
-  try {
-    var post = await replyModel.findOne({ _id: postID });
-    var user = await userModel.findOne({ _id: userID });
-  } catch (e) {
-    console.log(e.message);
-    await res.status(500).json("error detected");
-  }
-  if (!post.upvoteIds.includes(user._id)) {
-    try {
-      post.upvoteIds.push(user._id);
-    } catch (e) {
-      console.log(e.message);
-      await res.status(500).json("error detected");
-    }
-    if (post.downvoteIds.includes(user._id)) {
-      try {
-        post.downvoteIds.pop(user._id);
-      } catch (e) {
-        console.log(e.message);
-        await res.status(500).json("error detected");
-      }
-    };
-    await post.save();
-    await res.json(post);
-  } else {
-    try {
-      post.upvoteIds.pop(user._id);
-    } catch (e) {
-      console.log(e.message);
-      await res.status(500).json("error detected");
-    }
-    await post.save();
-    await res.json(post);
-  }
-}
-
-export async function downVoteReply1(req, res) {
-  const uidresponse = req.body.userID;
-  const postID = req.body.postID;
-  let userID = jwt.verify(uidresponse, process.env.SECRET)._id;
-  try {
-    var post = await replyModel.findOne({ _id: postID });
-    var user = await userModel.findOne({ _id: userID });
-  } catch (e) {
-    console.log(e.message);
-    await res.status(500).json("error detected");
-  }
-  if (!post.downvoteIds.includes(user._id)) {
-    try {
-      post.downvoteIds.push(user._id);
-    } catch (e) {
-      console.log(e.message);
-      await res.status(500).json("error detected");
-    }
-    if (post.upvoteIds.includes(user._id)) {
-      try {
-        post.upvoteIds.pop(user._id);
-      } catch (e) {
-        console.log(e.message);
-        await res.status(500).json("error detected");
-      }
-    };
-    await post.save();
-    await res.json(post);
-  } else {
-    try {
-      post.downvoteIds.pop(user._id);
-    } catch (e) {
-      console.log(e.message);
-      await res.status(500).json("error detected");
-    }
-    await post.save();
-    await res.json(post);
-  }
-}
-
-export async function CheckReply1(req, res){
-  const uidresponse = req.body.userID;
-  const postID = req.body.postID;
-  let userID = jwt.verify(uidresponse, process.env.SECRET)._id;
-  try {
-    var post = await replyModel.findOne({ _id: postID });
-    var user = await userModel.findOne({ _id: userID });
-  } catch (e) {
-    console.log(e.message);
-    await res.status(500).json("error detected");
-  }
-  if (post.downvoteIds.includes(user._id)){
-    res.json({value:-1})
-  }
-  else if (post.upvoteIds.includes(user._id)){
-    res.json({value: 1})
-  }
-  else{
-    res.json({value: 0})
   }
 }
--- routes/voteRoute.js
+++ routes/voteRoute.js
@@ -1,11 +1,8 @@
 import {Router} from "express";
-import {upVote, downVote, Check, upVoteReply1, downVoteReply1, CheckReply1} from "../controllers/voteController.js";
+import {upVote, downVote, Check} from "../controllers/voteController.js";
 const router = Router();
 
 router.post("/vote/up", upVote);
 router.post("/vote/down", downVote);
 router.post("/vote/check",Check);
-router.post("/reply1/vote/up", upVoteReply1);
-router.post("/reply1/vote/down", downVoteReply1);
-router.post("/reply1/vote/check", CheckReply1);
 export default router;
