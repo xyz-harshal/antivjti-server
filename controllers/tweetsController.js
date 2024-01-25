663cf016286a269cf5c8bc5df4f0292a4d177564
Soham Sunil Margaj <sohammargaj55555@gmail.com>
2024-01-25 09:39:49 +0530

    tweetscontroller

--- controllers/tweetsController.js
+++ controllers/tweetsController.js
@@ -23,11 +23,23 @@ export async function postTweets(req, res) {
 
 export async function getTweets(req,res){
   try{
+const authToken = req.headers['authorization'];
+    const userID = jwtVerify(authToken);
     let data=(await postModel.find({}));
     if(data.length>1){
       data=data.reverse()
     }
-    res.json(data)
+    let voteData = data.map((e)=>{
+      if (e.downvoteIds && e.downvoteIds.includes(userID)) {
+        return -1;
+      } else if (e.upvoteIds && e.upvoteIds.includes(userID)) {
+        return 1;
+      } else {
+        return 0;
+      }
+      
+    });
+    res.json({data, voteData});
   }
   catch(e){
     res.status(500).json({message:"all bad"});
