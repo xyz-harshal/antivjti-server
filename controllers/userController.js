024f8a7f0cdc5fbfbe3f1df21f40c8e3c424810f
harshal-ishere <harshal011kamble@gmail.com>
2024-01-31 21:07:30 +0530

    chill just a normal dev pushing

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,6 @@ import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken"
 const saltRounds = 10
 import getRandomFruitsName from "random-fruits-name"
-
 let createToken = (_id) => {
   return jwt.sign({ _id }, process.env.SECRET)
 }
@@ -13,7 +12,7 @@ export async function login(req, res) {
   try {
     let data = await userModel.findOne({ email: email })
     if (!data) {
-      res.json({ error: { email: false, password: false } })
+      res.json({ error: { email: false, password: false }})
     }
     if (data) {
       let pass = bcrypt.compareSync(password, data.password)
@@ -22,7 +21,7 @@ export async function login(req, res) {
         res.status(200).json({ email, token, error: { email: true, password: true } })
       }
       else {
-        res.json({ error: { email: true, password: false } });
+        res.json({ error: { email: true, password: false } })
       }
     }
   }
@@ -31,7 +30,6 @@ export async function login(req, res) {
     res.json({ status: e.message })
   }
 }
-
 export async function register(req, res) {
   let { email, password } = req.body
   try {
@@ -40,20 +38,16 @@ export async function register(req, res) {
       res.json({ error: true })
     }
     if (!data) {
-      const batch = email.match(/(\d+)/)
-      const branch = email.match(/@(\w+)/)
       const username = getRandomFruitsName()
-      const hash = bcrypt.hashSync(password, saltRounds);
+      let pass = bcrypt.hashSync(password, saltRounds)
       let user = new userModel({
         email: email,
-        password: hash,
+        password: pass,
         username: username,
-        batch: batch[0],
-        branch: branch[1]
       })
-      await user.save();
+      await user.save()
       let token = createToken(user._id)
-      res.status(200).json({ email, token, error: false });
+      res.status(200).json({ email,token, error: false })
     }
   }
   catch (e) {
